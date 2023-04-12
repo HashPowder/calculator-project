d30a6c0e755cd3089489c62b634736947bc2b8ce
HashPowder <benbra667@gmail.com>
2023-04-12 14:57:13 +0200

    Add additional functions to display number eight and the minus operator

--- javascript.js
+++ javascript.js
@@ -1,9 +1,25 @@
-const numberNine = 9;
+const numberNine = "9";
+const numberEight = "8";
+const operatorMinus = "-";
 let currentNumber = 0;
 
 
 
-function display() {
+function displayNine() {
     currentNumber = document.getElementById("display-text");
     currentNumber.innerText = numberNine;
+}
+
+function displayEight() {
+    currentNumber = document.getElementById("display-text");
+    currentNumber.innerText = numberEight;
+}
+
+function displayMinus () {
+    currentNumber = document.getElementById("display-text");
+    currentNumber.innerText = operatorMinus;
+}
+
+function calculate() {
+
 }
