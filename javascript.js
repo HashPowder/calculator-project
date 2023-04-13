06e549f7e0c80b120f4cca2eaf47d3a824ed5ba8
HashPowder <benbra667@gmail.com>
2023-04-13 10:11:23 +0200

    Undo how the numbers are called by the script and replace with a different method

--- javascript.js
+++ javascript.js
@@ -1,25 +1,7 @@
-const numberNine = "9";
-const numberEight = "8";
-const operatorMinus = "-";
+const nineStr = document.getElementById("btn-nine");
+const eightStr = "8";
+const minusStr = "-";
 let currentNumber = 0;
 
+alert(nineStr);
 
-
-function displayNine() {
-    currentNumber = document.getElementById("display-text");
-    currentNumber.innerText = numberNine;
-}
-
-function displayEight() {
-    currentNumber = document.getElementById("display-text");
-    currentNumber.innerText = numberEight;
-}
-
-function displayMinus () {
-    currentNumber = document.getElementById("display-text");
-    currentNumber.innerText = operatorMinus;
-}
-
-function calculate() {
-
-}
